66b5f66cedb52d68a95a0c334041c03203a759c4
Douglas José <douglassousawork@gmail.com>
2026-09-05 08:07:31 -0300

    criação de componentes

--- front-end/src/app/app.routes.ts
+++ front-end/src/app/app.routes.ts
@@ -1,9 +1,15 @@
 import { Routes } from '@angular/router';
 import { Login } from './pages/login/login';
 import { Dashboard } from './pages/dashboard/dashboard';
+import { Carrinho } from './pages/carrinho/carrinho';
+import { Perfil } from './perfil/perfil';
+import { Notificacoes } from './notificacoes/notificacoes';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' }, // Rota padrão
   { path: 'login', component: Login },
-  { path: 'dashboard', component: Dashboard }
+  { path: 'dashboard', component: Dashboard },
+  { path: 'carrinho', component: Carrinho},
+  { path: 'perfil', component: Perfil},
+  { path: 'notificacoes', component: Notificacoes},
 ];
