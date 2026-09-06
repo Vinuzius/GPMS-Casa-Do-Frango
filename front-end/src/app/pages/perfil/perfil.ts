6484c02b644467ae6966e8959dda10759ac2a0d0
Douglas José <douglassousawork@gmail.com>
2026-09-06 20:01:12 -0300

    View do perfil

--- front-end/src/app/pages/perfil/perfil.ts
+++ front-end/src/app/pages/perfil/perfil.ts
@@ -1,9 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { SHARED_IMPORTS } from '../../shared/shared-import';
+import { MATERIAL_IMPORTS } from '../../shared/material-imports';
+
+type ProfileSection = 'details' | 'addresses';
 
 @Component({
   selector: 'app-perfil',
-  imports: [],
+  imports: [...SHARED_IMPORTS, ...MATERIAL_IMPORTS],
   templateUrl: './perfil.html',
   styleUrl: './perfil.scss',
 })
-export class Perfil {}
+export class Perfil implements OnInit {
+  activeSection = signal<ProfileSection>('details');
+
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    this.route.queryParamMap.subscribe((params) => {
+      const section = params.get('secao');
+
+      if (section === 'enderecos') {
+        this.showAddresses();
+      } else if (section === 'dados') {
+        this.showDetails();
+      }
+    });
+  }
+
+  showDetails(): void {
+    this.activeSection.set('details');
+  }
+
+  showAddresses(): void {
+    this.activeSection.set('addresses');
+  }
+}
